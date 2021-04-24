04ce619d310364e5e85166a80ea45b5068dd7189
Viraj Panchal <virajpanchal8181@gmail.com>
2021-04-24 16:58:00 -0400

    Update route_planner.cpp

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -12,7 +12,6 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     // Store the nodes found in the RoutePlanner's start_node and end_node attributes.
    	start_node = &m_Model.FindClosestNode(start_x, start_y);
   	end_node = &m_Model.FindClosestNode(end_x, end_y); 
-
 }
 
 
